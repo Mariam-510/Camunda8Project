ad104fd88343a37069cb07c5c9152690cd30aee7
Mariam Ashraf <mariamashrafamin@gmail.com>
2025-09-05 16:52:13 +0300

    edit topic

---
+++ CamundaProject/CamundaProject.Application/Services/AccountOpening/GenerateApplicationIdJobWorkerService.cs
@@ -0,0 +1,21 @@
+﻿using CamundaProject.Application.Services.Kafka;
+using Confluent.Kafka;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Zeebe.Client;
+using Zeebe.Client.Api.Responses;
+using Zeebe.Client.Api.Worker;
+
+namespace CamundaProject.Application.Services.AccountOpening
+{
+    public class GenerateApplicationIdJobWorkerService
+    {
+    }
+}
