e0372a95c3e9fe95251cec96dcd252b5109d5ef5
Mostafa Samir <127770642+M0stafaSamir@users.noreply.github.com>
2025-09-06 00:26:57 +0300

    Create Account in Core

--- CamundaProject/CamundaProject.Application/Mapping/AutoMapperProfile.cs
+++ CamundaProject/CamundaProject.Application/Mapping/AutoMapperProfile.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using CamundaProject.Core.Models.ClientModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +12,19 @@ namespace CamundaProject.Application.Mapping
     {
         public AutoMapperProfile()
         {
+            CreateMap<ApplicationForm, BankAccount>()
+            .ForMember(dest => dest.AccountHolderName, opt => opt.MapFrom(src => src.FullName))
+            .ForMember(dest => dest.Balance, opt => opt.MapFrom(src => src.DepositAmount))
+            .ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(src => DateTime.UtcNow))
+            .ForMember(dest => dest.AccountNumber, opt => opt.MapFrom(src => GenerateAccountNumber()))
+            .ForMember(dest => dest.Id, opt => opt.Ignore()); 
+
+        }
+
+        private static string GenerateAccountNumber()
+        {
+            
+            return $"ACCT-{Guid.NewGuid().ToString().Substring(0, 8).ToUpper()}";
         }
     }
 }
---
+++ CamundaProject/CamundaProject.Core/Models/ClientModels/ApplicationForm.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CamundaProject.Core.Models.ClientModels
+{
+    public class ApplicationForm
+    {
+        public string ApplicationId { get; set; }
+        public string FullName { get; set; }
+        public string NationalId { get; set; }
+        public string ClientAddress { get; set; }
+        public decimal DepositAmount { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string NationalIdImage { get; set; }
+  
+    }
+}
